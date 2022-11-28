529046ac14999f9751097b2cc5bfb50717f48016
manavrbhatia <45702988+manavrbhatia@users.noreply.github.com>
2022-11-28 12:27:29 -0800

    Add files via upload

--- strassens_cpu.cpp
+++ strassens_cpu.cpp
@@ -89,64 +89,64 @@ void mat_multiply(double* a, double* b, double* mult, int d11, int d12, int d22)
         }
 
         // s1 = b12 - b22
-        sub(&b12, &b22, &s1, newSize);
+        sub(b12, b22, s1, newSize);
         
         // s2 = a11 + a12
-        add(&a11, &a12, &s2, newSize);
+        add(a11, a12, s2, newSize);
 
         // s3 = a21 + a22
-        add(&a21, &a22, &s3, newSize);
+        add(a21, a22, s3, newSize);
 
         // s4 = b21 - b11
-        sub(&b21, &b11, &s4, newSize);
+        sub(b21, b11, s4, newSize);
         
         // s5 = a11 + a22
-        add(&a11, &a22, &s5, newSize);
+        add(a11, a22, s5, newSize);
         
         // s6 = b11 + b22
-        add(&b11, &b22, &s6, newSize);
+        add(b11, b22, s6, newSize);
 
         // s7 = a12 - a22
-        sub(&a12, &a22, &s7, newSize);
+        sub(a12, a22, s7, newSize);
 
         // s8 = b21 + b22
-        add(&b21, &b22, &s8, newSize);
+        add(b21, b22, s8, newSize);
 
         // s9 = a11 - a21
-        sub(&a11, &a21, &s9, newSize);
+        sub(a11, a21, s9, newSize);
 
         // s10 = b11 + b12
-        add(&b11, &b12, &s10, newSize);
+        add(b11, b12, s10, newSize);
 
-        mat_multiply(&s7, &s8, &p1, newSize, newSize, newSize);
+        mat_multiply(s7, s8, p1, newSize, newSize, newSize);
 
-        mat_multiply(&s5, &s6, &p2, newSize, newSize, newSize);
+        mat_multiply(s5, s6, p2, newSize, newSize, newSize);
 
-        mat_multiply(&s9, &s10, &p3, newSize, newSize, newSize);
+        mat_multiply(s9, s10, p3, newSize, newSize, newSize);
 
-        mat_multiply(&s2, &b22, &p4, newSize, newSize, newSize);
+        mat_multiply(s2, b22, p4, newSize, newSize, newSize);
 
-        mat_multiply(&a11, &s1, &p5, newSize, newSize, newSize);
+        mat_multiply(a11, s1, p5, newSize, newSize, newSize);
 
-        mat_multiply(&a22, &s4, &p6, newSize, newSize, newSize);
+        mat_multiply(a22, s4, p6, newSize, newSize, newSize);
 
-        mat_multiply(&s3, &b11, &p7, newSize, newSize, newSize);
+        mat_multiply(s3, b11, p7, newSize, newSize, newSize);
 
         // c11 = p1 + p2 - p4 + p6
-        add(&p1, &p2, &tempA, newSize); // p1 + p2
-        add(&tempA, &p6, &tempB, newSize); // (p1 + p2) + p6
-        sub(&tempB, &p4, &c11, newSize); // (p5 + p4 + p6) - p2
+        add(p1, p2, tempA, newSize); // p1 + p2
+        add(tempA, p6, tempB, newSize); // (p1 + p2) + p6
+        sub(tempB, p4, c11, newSize); // (p5 + p4 + p6) - p2
 
         // c12 = p4 - p5
-        sub(&p1, &p5, &c12, newSize);
+        sub(p1, p5, c12, newSize);
 
         // c21 = p6 + p7
-        add(&p6, &p7, &c21, newSize);
+        add(p6, p7, c21, newSize);
 
         // c22 = p2 - p3 + p5 - p7
-        sub(&p2, &p3, &tempA, newSize); // p2 - p3
-        sub(&tempA, &p7, &tempB, newSize); // (p2 - p3) - p7
-        add(&tempB, &p5, &c22, newSize); // (p2 - p3 - p7) + p5
+        sub(p2, p3, tempA, newSize); // p2 - p3
+        sub(tempA, p7, tempB, newSize); // (p2 - p3) - p7
+        add(tempB, p5, c22, newSize); // (p2 - p3 - p7) + p5
 
         for (int i = 0; i < newSize ; i++) {
             for (int j = 0 ; j < newSize ; j++) {
